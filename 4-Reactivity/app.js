9f5cf7043059e52539bbb37b0d3c178e65bd9f71
Windy Arya <windy.205027@mhs.its.ac.id>
2022-09-30 09:34:26 +0700

    add task 4

---
+++ 4-Reactivity/app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+        }
+    },
+    watch: {
+        result() {
+            const that = this;
+            setTimeout(function() {
+                that.counter = 0;
+            }, 5000)
+        }
+    },
+    computed: {
+        result() {
+            if (this.counter < 37) {
+                return 'Not there yet';
+            } else if (this.counter === 37) {
+                return this.counter;
+            } else {
+                return 'Too much!';
+            }
+        }
+    },
+    methods: {
+        addCounter(num) {
+            this.counter = this.counter + num;
+        }
+    },
+});
+
+app.mount('#assignment')
