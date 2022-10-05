b4ac6416bfc4efc55c5df2d5a0e0a0f6033b821a
Windy Arya <windy.205027@mhs.its.ac.id>
2022-10-05 20:59:41 +0700

    add routing demo

---
+++ 13-Routing/src/components/nav/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>Page not found! Maybe view our <router-link to="/teams">Teams</router-link>?</h2>
+</template>
+
+<style scoped>
+    h2 {
+        text-align: center;
+    }
+</style>
